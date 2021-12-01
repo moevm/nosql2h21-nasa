339e231544a8aa3ca646778115a565658e1730d6
Alexander <alexandr-altukhov2013@yandex.ru>
2021-12-02 02:35:33 +0300

    фикс фильтров и пагинация

--- app/public/js/leaflet_map.js
+++ app/public/js/leaflet_map.js
@@ -1,21 +1,36 @@
 let url = 'http://api.open-notify.org/iss-now.json';
 
+var map;
+var ISSIcon;
+var ISSmarker;
+
 fetch(url)
 .then(res => res.json())
 .then((out) => {
-	console.log('Checkout this JSON! ', out);
-	var ISSIcon = L.icon({
+	ISSIcon = L.icon({
 		iconUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/International_Space_Station.svg/320px-International_Space_Station.svg.png',
 		iconSize:     [64, 64], // size of the icon
 		iconAnchor:   [32, 32], // point of the icon which will correspond to marker's location
 		popupAnchor:  [0, -32] // point from which the popup should open relative to the iconAnchor
 	});
-	var map = L.map('map').setView([out.iss_position.latitude, out.iss_position.longitude], 3);
+	map = L.map('map').setView([out.iss_position.latitude, out.iss_position.longitude], 3);
 
 	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 
-	L.marker([out.iss_position.latitude, out.iss_position.longitude], {icon: ISSIcon}).addTo(map);
+	ISSmarker = L.marker([out.iss_position.latitude, out.iss_position.longitude], {icon: ISSIcon}).addTo(map)
+	
 })
 .catch(err => { throw err });
+
+function map_update(){
+	fetch(url)
+	.then(res => res.json())
+	.then((out) => {
+		ISSmarker.setLatLng([out.iss_position.latitude, out.iss_position.longitude])
+	})
+	.catch(err => { throw err });
+}
+
+setInterval(map_update, 5000)
