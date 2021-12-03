8bc44dc3f3db105179ac2c520ee88633356c831b
Nikita Pochaev <pochaev.nik@gmail.com>
2021-12-03 23:11:43 +0300

    Optimize work and add new menu options
    Also some refactoring

--- app/public/js/table.pagination.js
+++ app/public/js/table.pagination.js
@@ -1,32 +1,37 @@
 var columns = {
-    name: 'Название',
-    recclass: 'Класс',
-    "mass (g)": 'Масса',
-	year: 'Год падения',
+    name: "Название",
+    recclass: "Класс",
+    mass: "Масса",
+    year: "Год падения",
 };
 
-var table = $('#table-sortable').tableSortable({
+var table = $("#table-sortable").tableSortable({
     data: [],
-	columns : columns,
+    columns: columns,
     rowsPerPage: 20,
     pagination: true,
 });
 
-
 var url = new URL(window.location.origin + "/service/query");
-$.get(url, function(data) {
+$.get(url, function (data) {
     table.setData(data, columns);
 });
 
-function tableUpdate(){
-	var url = new URL(window.location.origin + "/service/query?");
-	var recclass = document.getElementById("recclass2").value.toString();
-	console.log(recclass);
-	url.searchParams.append('recclass', recclass);
-	console.log(url.toString());
-	$.get(url, function(data) {
-		table.setData(data, columns);
-	});
-};	
+function tableUpdate() {
+    var url = new URL(window.location.origin + "/service/query?");
+    var recclass = document.getElementById("recclass").value.toString();
+    url.searchParams.append("recclass", recclass);
+    var fromYear = document.getElementById("fromYear").value.toString();
+    url.searchParams.append("fromYear", fromYear);
+    var toYear = document.getElementById("toYear").value.toString();
+    url.searchParams.append("toYear", toYear);
+    var fromMass = document.getElementById("fromMass").value.toString();
+    url.searchParams.append("fromMass", fromMass);
+    var toMass = document.getElementById("toMass").value.toString();
+    url.searchParams.append("toMass", toMass);
+    $.get(url, function (data) {
+        table.setData(data, columns);
+    });
+}
 
 document.getElementById("filter-button").onclick = tableUpdate;
