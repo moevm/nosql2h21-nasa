3247e66125a3e9c57c123b60d0af2a0267deac34
Nikita Pochaev <pochaev.nik@gmail.com>
2021-12-03 23:11:43 +0300

    Optimize work and add new menu options
    Also some refactoring

--- app/routes/meteo.js
+++ app/routes/meteo.js
@@ -1,45 +1,84 @@
-const {Router} = require('express')
-const landmet = require('../models/landmets')
-const router = Router()
-const http = require('http');
-const url = require('url');
+const { Router } = require("express");
+const landmet = require("../models/landmets");
+const router = Router();
+const http = require("http");
+const url = require("url");
 
-router.get('/', (req,res) => {
-    res.render('index')
-})
+router.get("/", (req, res) => {
+    res.render("index");
+});
 
-router.get('/landmet', async (req,res) => {
-    res.render('landmet')
-})
+router.get("/landmet", async (req, res) => {
+    res.render("landmet");
+});
 
 //returns query results
-router.get('/service/query', async (req,res) => {
-	const queryObject = url.parse(req.url,true).query;
-	if (queryObject.len){
-		const len = await landmet.find({recclass: queryObject.recclass}).count()
-		res.jsonp({len : len})
-	}
-	console.log(queryObject.recclass)
-    const landmets = await landmet.find(queryObject).lean()
-    console.log(landmets[0])
-    res.jsonp(landmets)
-})
+router.get("/service/query", async (req, res) => {
+    const queryObject = url.parse(req.url, true).query;
+    var queryFilter = {};
+    console.log(queryObject);
+    if (!queryObject.recclass) {
+        delete queryObject.recclass;
+    } else {
+        queryFilter.recclass = queryObject.recclass;
+    }
+    if (!queryObject.fromYear) {
+        delete queryObject.fromYear;
+    } else {
+        if (typeof queryFilter.year == "undefined") {
+            queryFilter.year = {};
+        }
+        queryFilter.year["$gte"] = parseInt(queryObject.fromYear);
+    }
+    if (!queryObject.toYear) {
+        delete queryObject.toYear;
+    } else {
+        if (typeof queryFilter.year == "undefined") {
+            queryFilter.year = {};
+        }
+        queryFilter.year["$lte"] = parseInt(queryObject.toYear);
+    }
+    if (!queryObject.fromMass) {
+        delete queryObject.fromMass;
+    } else {
+        if (typeof queryFilter.mass == "undefined") {
+            queryFilter.mass = {};
+        }
+        queryFilter.mass["$gte"] = parseInt(queryObject.fromMass);
+    }
+    if (!queryObject.toMass) {
+        delete queryObject.toMass;
+    } else {
+        if (typeof queryFilter.mass == "undefined") {
+            queryFilter.mass = {};
+        }
+        queryFilter.mass["$lte"] = parseInt(queryObject.toMass);
+    }
+    console.log(queryFilter);
+    if (queryObject.len) {
+        const len = await landmet.find(queryFilter).count();
+        res.jsonp({ len: len });
+    }
+    console.log(queryObject);
+    const landmets = await landmet.find(queryFilter).lean();
+    console.log(landmets[0]);
+    res.jsonp(landmets);
+});
 
 //returns query results count
-router.get('/service/query_len',  async (req,res) => {
-	const queryObject = url.parse(req.url,true).query;
-    const len = await landmet.find({recclass: queryObject.recclass}).count()
-    console.log(len)
-    res.jsonp({len : len})
-})
+router.get("/service/query_len", async (req, res) => {
+    const queryObject = url.parse(req.url, true).query;
+    const len = await landmet.find({ recclass: queryObject.recclass }).count();
+    console.log(len);
+    res.jsonp({ len: len });
+});
 
+router.get("/NEO", async (req, res) => {
+    res.render("NEO");
+});
 
-router.get('/NEO', async (req,res) => {
-  res.render('NEO')
-})
+router.get("/MKS", async (req, res) => {
+    res.render("MKS");
+});
 
-router.get('/MKS', async (req,res) => {
-    res.render('MKS')
-})
-
-module.exports = router
+module.exports = router;
